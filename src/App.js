ed710ab7e14c77e9ffe204424912a91733b3cced
Benjamin Craddock <benjamincraddock@benjamins-mbp.lan>
2022-03-03 13:55:14 -0500

    push

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 //components
 import Home from './components/Home';
 import UsersList from './components/Users';
 import User from './components/User';
+import Header from './components/Header';
+import Register from './components/Register';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import PrivateRoute from './components/PrivateRoute'
 
 function App() {
   return (
       <div className="App">
-        <Routes>
-          <Route path='/' element={<Home />} />
+        <Header/>
+        <Switch>
+          <Route exact path='/' element={<Home />} />
           <Route path='/users' element={<UsersList user={<User />}/>} />
-        </Routes>
+          <Route exact path='login' element={<Login />} />
+          <Route exact path='register' element={<Register />} />
+          <PrivateRoute exact path='/home' component={Home} />
+          <PrivateRoute exact path='/logout' component={Logout} />
+        </Switch>
       </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import axiosWithAuth from '../utils/axiosWithAuth';
+import url from './URL';
+
+const initialFormValues = { 
+  username: '',
+  password: ''
+}
+
+const errorValues = {
+  error: '',
+}
+
+const Login = () => {
+    const { push } = useHistory();
+    const [ formValues, setFormValues ] = useState(initialFormValues);
+    const [ error, setError ] = useState(errorValues);
+
+    const handleChange = (e) => {
+      setFormValues({
+        ...formValues,
+        [ e.target.name ]: e.target.value
+      });
+    }
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      axiosWithAuth().post(`${url}api/auth/login`, formValues)
+        .then(res => {
+          localStorage.setItem('token', res.data.token);
+          console.log(res);
+          // update push below to dashboard route when complete
+          push('/');
+        })
+        .catch(err => {
+          setError({
+            error: err.response.data.message          });
+        })
+      }
+
+    return(
+      <form className='items-list-wrapper' onSubmit={handleSubmit}>
+        <div className='register-wrapper'>            
+        <label>Username:
+          <input 
+            value={formValues.username}
+            name='username'
+            type='text'
+            placeholder='Username'
+            onChange={handleChange}
+          />
+        </label>
+        <label>Password:
+          <input 
+            value={formValues.password}
+            name='password'
+            type='password'
+            placeholder='Password'
+            onChange={handleChange}
+          />
+        </label>
+        <button>Log In</button>
+        <p>{error.error}</p>
+        </div>
+      </form>
+    )
+}
+
+export default Login;
